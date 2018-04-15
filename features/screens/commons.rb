ed002c42db23e88e38f61cd80188dde108f2f0d2
Bruno Carvalho <32099971+BrunoCarvalho18@users.noreply.github.com>
2018-04-15 20:12:40 -0300

    Update commons.rb

--- features/screens/commons.rb
+++ features/screens/commons.rb
@@ -2,10 +2,10 @@ class Home <Appium::Driver
 
 #===============================SCREEN===========================================#
       def initialize
-      @home_screen = find_element(:id, "imageView2")
+        @home_screen = find_element(:id, "imageView2")
       end
 #===============================METODOS===========================================#
       def telaprincipalaberta?
-      @home_screen
+        @home_screen
       end
 end
