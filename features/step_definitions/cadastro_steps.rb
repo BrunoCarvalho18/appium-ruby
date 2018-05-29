04eba5d46c9fd585ee3096780a1a41b93f6879a0
Luiz Pereira <lcnpj@outlook.com>
2018-05-29 17:20:20 -0300

    os metodos dentro da classe de steps devem receber as informacoes dos steps - Usar faker dentro do metodo limita o uso do metodo a sempre usar as informacoes criadas pelo faker

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -6,9 +6,9 @@ end
 Dado("prencho os campos correpondentes na tela de cadastro") do
   cadastro = Cadastro.new
   cadastro.btn_criarconta
-  cadastro.campo_preencher_nome
-  cadastro.campo_preencher_cpf
-  cadastro.campo_preencher_senha
+  cadastro.campo_preencher_nome(Faker::Name.first_name)
+  cadastro.campo_preencher_cpf(Faker::CPF.numeric)
+  cadastro.campo_preencher_senha(Faker::Internet.password(6,8))
   cadastro.clicar_btn_criar_conta
 end
 
