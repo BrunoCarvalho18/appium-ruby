5e3988692a5c6ee5a8673565148aa29451a802c0
Bruno Carvalho <32099971+BrunoCarvalho18@users.noreply.github.com>
2018-07-07 17:09:31 -0300

    Update cadastro_steps.rb

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -9,7 +9,7 @@ Dado("prencho os campos correpondentes na tela de cadastro") do
   cadastro.campo_preencher_nome(Faker::Name.first_name)
   cadastro.campo_preencher_cpf(Faker::CPF.numeric)
   cadastro.campo_preencher_senha(Faker::Internet.password(6,8))
-  cadastro.clicar_btn_criar_conta
+  .clicar_btn_criar_conta
 end
 
 Então("realizo o cadastro com sucesso") do
